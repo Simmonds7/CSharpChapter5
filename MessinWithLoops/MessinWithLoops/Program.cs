79e9a0b2b87cb44997d785ae00605bbaf77a8f36
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-30 13:06:52 -0600

    progrmas

---
+++ MessinWithLoops/MessinWithLoops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MessinWithLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i <= 9)
+            {
+                WriteLine(i);
+                i++;
+            }
+
+            while (i >= 0)
+            {
+                WriteLine(i);
+                i--;
+            }
+            do
+            {
+                WriteLine(i);
+                i++;
+            } while (i <= 9);
+            do
+            {
+                WriteLine(i);
+                i++;
+            } while (i >= 0);
+            for (int j = 0; j <= 9; ++j)
+            {
+                WriteLine(j);
+            }
+        }
+    }
+}
---
+++ ValidID/ValidID/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ValidID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idNum;
+            string input;
+            const int LOW = 1000;
+            const int HIGH = 9999;
+
+            Write("Enter Your employee ID number.");
+            input = ReadLine();
+            idNum = Convert.ToInt32(input);
+
+            while (idNum < LOW || idNum > HIGH)
+            {
+                WriteLine(idNum + " is an invalid ID number");
+                WriteLine("ID numbers must be, or between, " + LOW + " and " + HIGH);
+                input = ReadLine();
+                idNum = Convert.ToInt32(input);
+            }
+            WriteLine("ID Recognized, Welcome back valued Employee #" + idNum + ".");
+        }
+    }
+}
